f0dab2b0aaa1beeb5637c5f92945ea27b1f2c99a
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-06-20 23:47:13 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+let findMatching = (array,string) => {
+   let newArray = array.filter((item)=>{
+    return item.toUpperCase() === string.toUpperCase()
+   })
+   return newArray;
+}
+
+
+
+let fuzzyMatch = (array,string)=> {
+    let newValue = array.filter(item=>{
+       return  item.startsWith(string)
+    })
+    return newValue;
+}
+
+
+
+
+let matchName = (array,string)=>{
+    let newValue = array.filter(item =>{
+        if(item.name === string){
+            return item;
+        }
+    })
+    return newValue;
+}
+
